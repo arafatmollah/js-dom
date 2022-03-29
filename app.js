833b41ad085d07c32b3c17ce737a99190ca3c3c5
Arafat MShuvo <arafatshuvo18@gmail.com>
2022-03-29 23:57:06 +0600

    commited

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+function checkButton() {
+    const button = document.getElementById('btn');
+    const sec = document.getElementById('section');
+    sec.style.fontSize = '25px';
+    document.body.style.backgroundColor = 'red';
+    document.body.style.color = 'white';
+}
